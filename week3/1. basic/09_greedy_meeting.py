b07a93e9f375e75df771ddb5091c4caf19dc0276
Junghee Kim <a01030704572@gmail.com>
2026-09-07 22:48:44 +0900

    Implement greedy meeting scheduling

--- week3/1. basic/09_greedy_meeting.py
+++ week3/1. basic/09_greedy_meeting.py
@@ -33,19 +33,24 @@ def select_meetings(meetings):
         (배정된 회의 개수, 선택된 회의 리스트)
     """
     # TODO: 회의가 없으면 0 반환
-    pass
-    
-    # TODO: 종료 시간 기준으로 정렬
-    pass
-    
     selected = []
+    if meetings == []:
+        return 0, selected
+    # TODO: 종료 시간 기준으로 정렬
+    meetings.sort(key=lambda meetings: (meetings[1],meetings[0]))
+    
     
     # TODO: 첫 번째 회의 선택
-    pass
+    selected.append(meetings[0])
+    last_end = meetings[0][1]
+    for start,end in meetings[1:]:
+        if start >= last_end:
+            selected.append((start,end))
+            last_end = end    
     
     # TODO: 나머지 회의들 확인
     ## 이전 회의가 끝난 후 시작하는 회의만 선택
-    pass
+            
     
     return len(selected), selected
 
