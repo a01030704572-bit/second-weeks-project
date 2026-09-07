fd43ef4c9362ea6ee0d44f050d290574088a4000
Junghee Kim <a01030704572@gmail.com>
2026-09-07 20:35:41 +0900

    Implement greedy coin change

--- week3/1. basic/08_greedy_coin.py
+++ week3/1. basic/08_greedy_coin.py
@@ -43,12 +43,20 @@ def make_change_greedy(change, coins):
     result = {}
     total_coins = 0
     
+    
     # TODO: 각 동전에 대해 반복
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
-    pass
-    
+    for i in coins:
+        total = change//i
+        total_coins = total_coins+total
+        change= change - i * total
+        if total > 0:
+            result[i] = total
     return total_coins, result
+
+            
+      
 
 # 테스트 케이스
 if __name__ == "__main__":
